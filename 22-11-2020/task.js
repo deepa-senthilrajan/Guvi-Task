2d0572abfd84ad577fa475ff0fbbce66133bb539
deepa-senthilrajan <73987900+deepa-senthilrajan@users.noreply.github.com>
2020-11-23 16:43:31 +0530

    Update task.js

--- 22-11-2020/task.js
+++ 22-11-2020/task.js
@@ -1,11 +1,22 @@
-#filter countries coming under region asia
+//Get all the countries from Asia continent using Filter function
 
 const filtereditems = countries.filter((item)=>{
     return item.region = 'asia'
 })
 
-#display country names from filtereditems array
+
 const names = filtereditems.map((item) => {
     return item.name;
 })
 console.log(names)
+
+//Get all the countries with population of less than 2 lacs using Filter function 
+
+const popitems = countries.filter((item) =>{
+    return item.population <200000
+})
+const popnames = popitems.map((item)=>{
+    return item.name;
+})
+
+console.log(popnames)
